13814b5eab9b81560d013ef531df571ce44a3254
dongyeonkim <ehd9567@naver.com>
2023-01-27 18:47:11 +0900

    DOM내용

--- src/main/resources/static/javascript/js_example/jhonExample.js
+++ src/main/resources/static/javascript/js_example/jhonExample.js
@@ -1,4 +1,9 @@
-// 자바스크립트 객체를 가져오는 getElementById
+// DOMContentLoaded란 이벤트 파일이 로딩될때 하단 function이벤트가 실행된다.
+document.addEventListener("DOMContentLoaded",
+    function (event) {
+      // 어떤 이벤트인지 확인이 가능하다
+      console.log(event);
+      // 자바스크립트 객체를 가져오는 getElementById
 // console.log(document.getElementById("title"));
 // console.log(document instanceof HTMLDocument);
 
@@ -8,21 +13,36 @@
 // .VALUE는 input으로 사용
 //   console.log(document.getElementById("name").value);
 // }
+      function sayHello(event) {
+        // this는 하단 클릭시 발생하는 addEventListener를 의미함
+        console.log(this);
+        var name = document.getElementById("name").value;
+        var message = "<h2>Hello" + name + "!</h2>";
+        // textContent로 텍스트 값을 넣어줄수 있음
+        // document.getElementById("content").textContent = message;
+        // HTML로 해석해서 값을 넣어줄때 innerHTML
+        document.getElementById("content").innerHTML = message;
 
-function sayHello(){
-  var name = document.getElementById("name").value;
-  var message = "<h2>Hello" + name + "!</h2>";
-  // textContent로 텍스트 값을 넣어줄수 있음
-  // document.getElementById("content").textContent = message;
-  // HTML로 해석해서 값을 넣어줄때 innerHTML
-  document.getElementById("content").innerHTML = message;
+        if (name === "student") {
+          // getElementById는 페이지 내에서 id로 선택에 제약이 있으므로 css 셀렉터인 querySelector를 사용
+          // querySelector로 인한 #title
+          var title = document.querySelector("#title").textContent;
+          title += " & LOVE It";
+          // 명시적으로 어떤값이 입력될지를 = 를통해 넣어준다.
+          document.querySelector("#title").textContent = title;
+        }
+      }
 
-  if (name === "student"){
-    // getElementById는 페이지 내에서 id로 선택에 제약이 있으므로 css 셀렉터인 querySelector를 사용
-    // querySelector로 인한 #title
-    var title = document.querySelector("#title").textContent;
-    title += " & LOVE It";
-    // 명시적으로 어떤값이 입력될지를 = 를통해 넣어준다.
-    document.querySelector("#title").textContent = title;
-  }
-}
+// HTML과 이벤트를 분리해서 HTML은 어떤 이벤트가 있는지 알수없음
+      document.querySelector("button").addEventListener("click", sayHello);
+
+      //  새로운 이벤트 작성시에도 틀에 맞춰서 작성할수있다
+      document.querySelector("body").addEventListener("mousemove",
+          function (event) {
+            if (event.shiftKey === true) {
+              console.log("x: " + event.clientX);
+              console.log("Y: " + event.clientY);
+            }
+          })
+    }
+);
